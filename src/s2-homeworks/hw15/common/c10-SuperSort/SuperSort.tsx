316705d01fc5820384ac046ec0b3aad2d400c632
Lecser <107482432+Lecser@users.noreply.github.com>
2023-04-12 01:37:56 +0300

    done hw15

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,54 +1,37 @@
-import React from 'react'
-
-// добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+import React from "react";
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
-}
+  id?: string;
+  sort: string;
+  value: string;
+  onChange: (newSort: string) => void;
+};
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
-}
+  // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+  return sort === "" ? down : sort === down ? up : sort === up ? "" : down;
+};
 
-const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
-) => {
-    const up = '0' + value
-    const down = '1' + value
+const SuperSort: React.FC<SuperSortPropsType> = ({
+  sort,
+  value,
+  onChange,
+  id = "hw15",
+}) => {
+  const up = "0" + value;
+  const down = "1" + value;
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+  const onChangeCallback = () => {
+    onChange(pureChange(sort, down, up));
+  };
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+  return (
+    <span id={id + "-sort-" + value} onClick={onChangeCallback}>
+      {/*сделать иконку*/}
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
+      {/*{icon} /!*а это убрать*!/*/}
+    </span>
+  );
+};
 
-            {icon} {/*а это убрать*/}
-        </span>
-    )
-}
-
-export default SuperSort
+export default SuperSort;
